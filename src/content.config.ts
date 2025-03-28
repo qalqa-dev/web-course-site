b77b12ce9979e4028ec71d0bff3a65f70c809884
qalqa-dev <andreybas04@gmail.com>
2025-03-29 01:51:33 +0300

    refactor(course-layout): centralize course data fetching in CourseLayout

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,5 +1,7 @@
 import { glob } from "astro/loaders";
 import { defineCollection } from "astro:content";
+import { api } from "./data/api.js";
+import { Course } from "./types/entities/Courses.js";
 
 const courses = defineCollection({
     loader: glob({ pattern: "**/*.md", base: "./src/courses" }),
@@ -9,4 +11,14 @@ const useful = defineCollection({
     loader: glob({ pattern: "**/*.md", base: "./src/useful" }),
 });
 
-export const collections = { courses, useful };
+const coursesServer = defineCollection({
+    loader: async () => {
+        const courses = await api.getData<Course[]>("courses");
+        return courses.map((course) => ({
+            id: course.id,
+            data: course,
+        }));
+    },
+});
+
+export const collections = { courses, useful, coursesServer };
