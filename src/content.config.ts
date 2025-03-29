7efd31aa05d2a8c8e0a88798ccdd14b8cc6fd3f5
qalqa-dev <andreybas04@gmail.com>
2025-03-29 03:56:54 +0300

    refactor(pages): added dynamic route generation for courses

--- src/content.config.ts
+++ src/content.config.ts
@@ -3,22 +3,19 @@ import { defineCollection } from "astro:content";
 import { api } from "./data/api.js";
 import { Course } from "./types/entities/Courses.js";
 
-const courses = defineCollection({
-    loader: glob({ pattern: "**/*.md", base: "./src/courses" }),
-});
-
 const useful = defineCollection({
     loader: glob({ pattern: "**/*.md", base: "./src/useful" }),
 });
 
-const coursesServer = defineCollection({
+const courses = defineCollection({
     loader: async () => {
         const courses = await api.getData<Course[]>("courses");
-        return courses.map((course) => ({
-            id: course.id,
-            data: course,
-        }));
+        return courses.map((course) => {
+            return {
+                ...course,
+                id: course.name,
+            };
+        });
     },
 });
-
-export const collections = { courses, useful, coursesServer };
+export const collections = { courses, useful };
